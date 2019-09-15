22e100c9b2315f52d43e11cfe125848d25e9ddf4
Avaisuddin Mohammad <avaisuddin.mohammad@outlook.com>
2019-09-15 22:56:45 +0100

    WFH - Observables WIP

--- src/app/modules/observables/components/home-page/home-page.component.ts
+++ src/app/modules/observables/components/home-page/home-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, fromEvent, of, } from 'rxjs';
+import { filter, map, catchError, retry } from 'rxjs/operators';
+import { NumberSymbol } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
@@ -10,10 +12,23 @@ export class HomePageComponent implements OnInit {
 
   simpleObservable: number[] = [];
 
+  mouseX: number;
+  mouseY: number;
+
+  oddNumberSquares: number[] = [];
+  evenNumberSquares: number[] = [];
+
+  observableString: string;
+
+  observableRetryString: string;
+
   constructor(private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.loadSimpleObservable();
+    this.evenOddNumberTransformsWithPipeFilterAndMap();
+    this.errorsAfterOneSec();
+    this.retryAfterErrors();
   }
 
   loadSimpleObservable() {
@@ -50,4 +65,107 @@ export class HomePageComponent implements OnInit {
     }, () => { console.log('simple observable completed!'); });
 
   }
+
+  startListeningEventMovement(event: MouseEvent) {
+    const fromEventObs = fromEvent(event.target, 'mousemove');
+
+    const subs = fromEventObs.subscribe((evt: MouseEvent) => {
+
+      this.mouseX = evt.clientX;
+      this.mouseY = evt.clientY;
+
+      if (evt.clientX < 40 && evt.clientY < 40) {
+        subs.unsubscribe();
+      }
+
+    });
+
+  }
+
+  evenOddNumberTransformsWithPipeFilterAndMap() {
+    const numbrs = new Observable((observer) => {
+      let i = 1;
+      const intr = setInterval(() => {
+        if (i === 7) {
+          observer.complete();
+          clearInterval(intr);
+        }
+        observer.next(i++);
+      }, 1000);
+    });
+
+    //of(1, 2, 3, 4, 5, 6);
+    numbrs.pipe(
+      filter((num: number) => num % 2 !== 0),
+      map(n => n * n)
+    ).subscribe(n => {
+      const temp = Object.assign([], this.oddNumberSquares);
+      temp.push(n);
+      this.oddNumberSquares = Object.assign([], temp);
+    });
+
+    numbrs.pipe(
+      filter((num: number) => num % 2 === 0),
+      map(n => n * n)
+    ).subscribe(n => {
+      const temp = Object.assign([], this.evenNumberSquares);
+      temp.push(n);
+      this.evenNumberSquares = Object.assign([], temp);
+    });
+  }
+
+  errorsAfterOneSec() {
+    const observable = new Observable((observer) => {
+      let i = 1;
+      const intr = setInterval(() => {
+
+        if (i === 5) {
+          clearInterval(intr);
+          observer.error(new Error('deliberate exception'));
+          //observer.next(5 / 0);
+        }
+        observer.next(i++);
+      }, 1000);
+    })
+      .pipe(
+        map(n => { if (n === 6) { throw new Error('First Error'); } else { return n; } }),
+        catchError(err => of(err))
+      );
+
+    observable.subscribe((p: any) => {
+      this.observableString = p.toString();
+    }, error => {
+      console.log(error);
+      this.observableString = error;
+    });
+
+  }
+
+  retryAfterErrors() {
+    const observable = new Observable((observer) => {
+      let i = 1;
+      const intr = setInterval(() => {
+
+        if (i === 3) {
+          clearInterval(intr);
+          observer.error(new Error('deliberate exception'));
+          //observer.next(5 / 0);
+        }
+        observer.next(i++);
+      }, 1000);
+    }).pipe(
+      retry(2),
+      map(n => n),
+      catchError(err => of(err))
+    );
+
+    observable.subscribe((p: any) => {
+      this.observableRetryString = p.toString();
+    }, error => {
+      console.log(error);
+      this.observableRetryString = error;
+    });
+
+  }
+
 }
